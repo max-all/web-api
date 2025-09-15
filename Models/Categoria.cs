c4d0fb5264edcafbf06c4bb4e88965602679c516
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-09-14 21:01:01 -0300

    added models `Produto` and `Categoria`

---
+++ Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace web_Api.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+
+
+    public ICollection<Produto>? Produtos { get; set; }
+}   
---
+++ Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace web_Api.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public decimal Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+
+    public int? CategoryId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
